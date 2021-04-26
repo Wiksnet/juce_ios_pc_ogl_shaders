1488224600eab9add6308700943f6b2f5772696a
Wiksnet <vitalytarasuk@gmail.com>
2021-04-26 22:07:35 +0300

    Create simple_gl_component.h

---
+++ simple_gl_component.h
@@ -0,0 +1,51 @@
+//
+// Created by Vitaly Tarasuk on 25.04.2021.
+//
+
+#pragma once
+
+#include <JuceHeader.h>
+
+using namespace juce;
+
+class SimpleGLComponent : public OpenGLAppComponent {
+public:
+    SimpleGLComponent();
+
+    ~SimpleGLComponent() override;
+
+    void initialise() override;
+
+    void shutdown() override;
+
+    void render() override;
+
+    void newOpenGLContextCreated() override;
+
+    void renderOpenGL() override;
+
+    void openGLContextClosing() override;
+
+    void paint (Graphics& g) override;
+
+    void parentHierarchyChanged() override;
+
+private:
+    struct Vertex
+    {
+        float position[2];
+        float sourceColour[4];
+    };
+
+    std::vector<Vertex> vertexBuffer;
+    std::vector<unsigned int> indexBuffer;
+
+    GLuint vbo; // Vertex buffer object.
+    GLuint ibo; // Index buffer object.
+
+    String vertexShader;
+    String fragmentShader;
+
+    std::unique_ptr<OpenGLShaderProgram> shaderProgram;
+};
+
